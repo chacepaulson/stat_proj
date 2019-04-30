dcd1179e73e6b34c00d28e12d7302ba42a21ba6b
chacepaulson <paulson.chace@gmail.com>
2019-04-30 11:51:56 -0400

    add project code

---
+++ project_code/calculate_means.R
@@ -0,0 +1,19 @@
+# import data 
+library(readr)
+winner_calc2 <- read_csv("~/Desktop/stat_proj/coded_data/winner_calc2.csv", 
+                         col_types = cols(X1 = col_skip()))
+loser_calc2 <- read_csv("~/Desktop/stat_proj/coded_data/loser_calc2.csv", 
+                        col_types = cols(X1 = col_skip()))
+
+
+
+
+
+
+
+
+
+
+
+
+
